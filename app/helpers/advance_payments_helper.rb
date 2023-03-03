056ad18c8817390d5c5d798c27a32d476f012ee4
PepeLira <jflira@miuandes.cl>
2023-03-03 02:13:44 -0300

    feat: asign the advance status badge color

--- app/helpers/advance_payments_helper.rb
+++ app/helpers/advance_payments_helper.rb
@@ -1,2 +1,12 @@
 module AdvancePaymentsHelper
+  def status_badge_color(status)
+    case status
+    when "Rechazado"
+      'badge badge-danger'
+    when "Aprobado"
+      'badge badge-success'
+    else
+      'badge badge-warning'
+    end
+  end
 end
